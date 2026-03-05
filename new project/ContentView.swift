fabb713e7f3b6b4adfb2e26a0fa530396b7bdf81
Muskan Shafiq-coder <muskanshafiq@gmail.com>
2026-03-05 17:39:34 +0500

    Initial Commit

---
+++ new project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  new project
+//
+//  Created by Muhammad Ahsan on 05/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ new project/new_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_projectApp.swift
+//  new project
+//
+//  Created by Muhammad Ahsan on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct new_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
